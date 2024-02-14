754d0f8dc33147ade076fd2c1390979336effc89
DINESH <dineshpuyal007@gmail.com>
2024-02-14 09:30:10 +0530

    Id duplicate issue for sprits

--- src/component/AnimationBoard.tsx
+++ src/component/AnimationBoard.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as RN from 'react-native';
-import AnimationPoster from './AnimationPoster';
-import {AppContext} from '../../App';
-import Icon from 'react-native-vector-icons/Ionicons';
+import * as React from "react";
+import * as RN from "react-native";
+import AnimationPoster from "./AnimationPoster";
+import { AppContext } from "../../App";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const AnimationBoard = () => {
   let animationPosterRef: any = React.useRef([]);
@@ -21,18 +21,19 @@ const AnimationBoard = () => {
         appData?.selectedMotion?.map((item: any) => {
           animationPosterRef[item?.id]?.resetAnimation();
         });
-      }}>
-      <Icon name="stop-outline" size={20} color={'white'} />
+      }}
+    >
+      <Icon name="stop-outline" size={20} color={"white"} />
     </RN.TouchableOpacity>
   );
 
   const renderPlayBtn = () => (
     <RN.TouchableOpacity style={styles.playBtn} onPress={playAnimation}>
-      <Icon name="play-circle-outline" size={20} color={'white'} />
+      <Icon name="play-circle-outline" size={20} color={"white"} />
     </RN.TouchableOpacity>
   );
 
-  const renderItem = React.useCallback(({item, index}: any) => {
+  const renderItem = React.useCallback(({ item, index }: any) => {
     return (
       <AnimationPoster
         data={item}
@@ -47,7 +48,7 @@ const AnimationBoard = () => {
 
   const keyExtractor = React.useCallback(
     (item: any, index: any) => item?.id?.toString(),
-    [],
+    []
   );
 
   const renderCreature = () => (
@@ -55,7 +56,7 @@ const AnimationBoard = () => {
       data={appData?.selectedMotion}
       keyExtractor={keyExtractor}
       renderItem={renderItem}
-      contentContainerStyle={{flex: 1}}
+      contentContainerStyle={{ flex: 1 }}
     />
   );
 
@@ -71,7 +72,7 @@ const AnimationBoard = () => {
 const styles = RN.StyleSheet.create({
   animationBoard: {
     flex: 0.95,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 7,
     margin: 7,
     elevation: 5,
@@ -80,11 +81,11 @@ const styles = RN.StyleSheet.create({
   resetBtn: {
     height: 30,
     width: 30,
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
     top: 10,
     right: 10,
     zIndex: 10,
@@ -92,11 +93,11 @@ const styles = RN.StyleSheet.create({
   playBtn: {
     height: 30,
     width: 30,
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
     bottom: 10,
     right: 10,
     zIndex: 10,
--- src/navigation/MyTabs.tsx
+++ src/navigation/MyTabs.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import * as RN from 'react-native';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import Motion from '../screens/motion/motion';
-import Looks from '../screens/looks/Looks';
-import Control from '../screens/control/Control';
-import Events from '../screens/event/Event';
-import {useNavigation} from '@react-navigation/native';
+import * as React from "react";
+import * as RN from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Motion from "../screens/motion/motion";
+import Looks from "../screens/looks/Looks";
+import Control from "../screens/control/Control";
+import Events from "../screens/event/Event";
+import { useNavigation } from "@react-navigation/native";
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -19,7 +19,8 @@ const MyTabs = (props: any) => {
           style={styles.doneBtn}
           onPress={() => {
             navigation.goBack();
-          }}>
+          }}
+        >
           <RN.Text style={styles.doneBtnText}>Done</RN.Text>
         </RN.TouchableOpacity>
       ),
@@ -29,11 +30,12 @@ const MyTabs = (props: any) => {
   return (
     <Tab.Navigator
       screenOptions={{
-        tabBarLabelStyle: {fontSize: 12, width: '100%', color: 'white'},
-        tabBarStyle: {backgroundColor: 'blue'},
+        tabBarLabelStyle: { fontSize: 12, width: "100%", color: "white" },
+        tabBarStyle: { backgroundColor: "blue" },
         swipeEnabled: false,
-        tabBarIndicatorStyle: {backgroundColor: 'white'},
-      }}>
+        tabBarIndicatorStyle: { backgroundColor: "white" },
+      }}
+    >
       <Tab.Screen name="Motion" component={Motion} />
       <Tab.Screen name="Looks" component={Looks} />
       <Tab.Screen name="Control" component={Control} />
@@ -44,12 +46,12 @@ const MyTabs = (props: any) => {
 
 const styles = RN.StyleSheet.create({
   doneBtn: {
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
     paddingHorizontal: 7 * 2,
     paddingVertical: 7 / 1.5,
     borderRadius: 7,
   },
-  doneBtnText: {color: 'white'},
+  doneBtnText: { color: "white" },
 });
 
 export default MyTabs;
